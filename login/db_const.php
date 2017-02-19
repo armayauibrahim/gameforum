dca6322c5b294f99f8de552d9ea380bec0bd7c5b
armayauibrahim <arma_one@yahoo.com>
2017-02-19 19:13:47 +0000

    Add files via upload

---
+++ login/db_const.php
@@ -0,0 +1,9 @@
+<?php
+
+	
+	# mysql db constants DB_HOST, DB_USER, DB_PASS, DB_NAME
+	const DB_HOST = 'localhost';
+	const DB_USER = 'root';
+	const DB_PASS = '';
+	const DB_NAME = 'game_forum';
+?>
