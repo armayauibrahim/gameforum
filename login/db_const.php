5ef380c4afd2ff1d3e083aaec519efe91c380b4f
armayauibrahim <a.ibrahim@rgu.ac.uk>
2017-02-23 12:39:01 +0000

    add adminer

--- login/db_const.php
+++ login/db_const.php
@@ -2,8 +2,8 @@
 
 	
 	# mysql db constants DB_HOST, DB_USER, DB_PASS, DB_NAME
-	const DB_HOST = 'localhost';
-	const DB_USER = 'root';
-	const DB_PASS = '';
-	const DB_NAME = 'game_forum';
+	const DB_HOST = 'ap-cdbr-azure-east-a.cloudapp.net';
+	const DB_USER = 'b352698ef6d21b';
+	const DB_PASS = '547d321e';
+	const DB_NAME = 'gameforum';
 ?>
